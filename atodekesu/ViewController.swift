a37997ba92ec91d8ba32b49ebe147232cd287cef
Clgoggle <misaryou.0404.0813@gmail.com>
2024-07-28 13:43:29 +0900

    Initial Commit

---
+++ atodekesu/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  atodekesu
+//
+//  Created by goggle CL on 2024/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var Label:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
