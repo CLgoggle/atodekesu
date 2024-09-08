09cdcf9ab3fb3dde21460feb4738ec07301e4d24
Clgoggle <misaryou.0404.0813@gmail.com>
2024-09-08 10:35:18 +0900

    初動あるのに誘発しか引かないのなんなん

---
+++ atodekesu/AddTweetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddTweetViewController.swift
+//  atodekesu
+//
+//  Created by goggle CL on 2024/08/25.
+//
+
+import UIKit
+
+class AddTweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ atodekesu/Tweet.swift
@@ -0,0 +1,8 @@
+//
+//  Tweet.swift
+//  atodekesu
+//
+//  Created by goggle CL on 2024/08/25.
+//
+
+import Foundation
